7ec8b71dd03ea4a1f8a2ed76ad0d9de33e1d3539
vs-darsh-viroja <darsh.vsquare.team@gmail.com>
2025-10-11 11:46:34 +0530

    Initial Commit

---
+++ glbq/glbqApp.swift
@@ -0,0 +1,17 @@
+//
+//  glbqApp.swift
+//  glbq
+//
+//  Created by Purvi Sancheti on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct glbqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
